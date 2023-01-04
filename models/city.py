92a266a557c1eaead61d14d91f363c36836a0cdb
Endris M <EndrisMd@gmail.com>
2023-01-04 23:51:05 +0300

    DBStorage -City update

--- models/city.py
+++ models/city.py
@@ -1,23 +1,21 @@
 #!/usr/bin/python3
-""" City Module for HBNB project """
+"""This is the city class"""
+from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String
+from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship
-import os
+from models.place import Place
 
 
 class City(BaseModel, Base):
-        """This is the class for City
+    """This is the class for City
     Attributes:
         state_id: The state id
         name: input name
     """
     __tablename__ = "cities"
-
-    if os.getenv("HBNB_TYPE_STORAGE") == "db":
-        name = Column(String(128), nullable=False)
-        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-        places = relationship("Place", cascade="all")
-    else:
-        state_id = ""
-        name = ""
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    places = relationship("Place", cascade='all, delete, delete-orphan',
+                          backref="cities")
