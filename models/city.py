22ff271d430acf15af1e51539116abbf3e369cbd
Endris M <EndrisMd@gmail.com>
2023-01-05 21:30:33 +0300

    DBStorage - City class update

--- models/city.py
+++ models/city.py
@@ -14,8 +14,15 @@ class City(BaseModel, Base):
         state_id: The state id
         name: input name
     """
-    __tablename__ = "cities"
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    places = relationship("Place", cascade='all, delete, delete-orphan',
-                          backref="cities")
+    if storage_type == 'db':    
+        __tablename__ = "cities"
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        places = relationship("Place", cascade='all, delete, delete-orphan', backref="cities")
+    else:
+        state_id = ""
+        name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes city"""
+        super().__init__(*args, **kwargs)
