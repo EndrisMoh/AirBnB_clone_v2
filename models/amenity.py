9f49216eda9e0b9cf381d774a4434e338f96e0cc
Endris M <EndrisMd@gmail.com>
2023-01-05 21:08:48 +0300

    DBStorage - Amenity class update

--- models/amenity.py
+++ models/amenity.py
@@ -5,3 +5,18 @@ from models.base_model import BaseModel
 
 class Amenity(BaseModel):
     name = ""
+
+#!/usr/bin/python3
+""" State Module for HBNB project """
+from models.base_model import BaseModel, Base
+from models import storage_type
+from sqlalchemy import Column, String
+
+
+class Amenity(BaseModel, Base):
+    '''amenity class'''
+    __tablename__ = 'amenities'
+    if storage_type == 'db':
+        name = Column(String(128), nullable=False)
+    else:
+        name = ""
