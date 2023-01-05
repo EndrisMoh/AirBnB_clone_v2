9a5a78b8a1eca2b54ea86a903b66fc583d16ed52
Endris M <EndrisMd@gmail.com>
2023-01-05 21:41:59 +0300

    DBStorage - Place class update

--- models/place.py
+++ models/place.py
@@ -1,24 +1,5 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel
-
-
-class Place(BaseModel):
-    """ A place to stay """
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
-    amenity_ids = []
-
-#!/usr/bin/python3
-""" Place Module for HBNB project """
 
 from models.amenity import Amenity
 from models.review import Review
