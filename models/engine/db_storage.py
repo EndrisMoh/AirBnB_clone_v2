11cac9d70bdd9c2efe60a8d21dfaee44c44f912c
EndrisMoh <endrismd@gmail.com>
2023-01-09 12:03:16 -0800

    BaseModel added

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -5,7 +5,7 @@ import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 from models.amenity import Amenity
-from models.base_model import Base
+from models.base_model import BaseModel, Base
 from models.city import City
 from models.place import Place
 from models.review import Review
